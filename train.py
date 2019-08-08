2572a7a841d7bc1bf14948820c68152991a169ac
ace19-dev <acemc19@gmail.com>
2019-08-08 13:05:18 +0900

    update

--- train.py
+++ train.py
@@ -385,7 +385,7 @@ def main(unused_argv):
                                      X: train_batch_xs,
                                      ground_truth: train_batch_ys,
                                      is_training: True,
-                                     keep_prob: 0.7
+                                     keep_prob: 0.8
                                  })
                     train_writer.add_summary(train_summary, num_epoch)
                     # train_writer.add_summary(grad_vals, num_epoch)
